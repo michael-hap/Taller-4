c617775873b605fbee01729ba5f6517fed8763a5
Michael <michaelh.acevedop@uqvirtual.edu.co>
2026-05-02 18:43:45 -0500

    Agregar gestión de clases en Gimnasio

--- lib/gimnasio.ex
+++ lib/gimnasio.ex
@@ -39,4 +39,54 @@ defmodule GimnasioApp.Gimnasio do
   def listar_socios(socios) do
     {:ok, Map.values(socios)}
   end
+
+  def inscribir_clase(socios, cedula, clase) do
+    case Map.get(socios, cedula) do
+      nil ->
+        {:error, :no_encontrado}
+
+      socio ->
+        case Socio.inscribir_clase(socio, clase) do
+          {:ok, socio_actualizado} ->
+            socios_actualizados = Map.put(socios, cedula, socio_actualizado)
+            {:ok, socios_actualizados}
+
+          {:error, razon} ->
+            {:error, razon}
+        end
+    end
+  end
+
+  def desinscribir_clase(socios, cedula, clase) do
+    case Map.get(socios, cedula) do
+      nil ->
+        {:error, :no_encontrado}
+
+      socio ->
+        case Socio.desinscribir_clase(socio, clase) do
+          {:ok, socio_actualizado} ->
+            socios_actualizados = Map.put(socios, cedula, socio_actualizado)
+            {:ok, socios_actualizados}
+        end
+    end
+  end
+
+  def listar_clases_socio(socios, cedula) do
+    case Map.get(socios, cedula) do
+      nil ->
+        {:error, :no_encontrado}
+
+      socio ->
+        Socio.listar_clases(socio)
+    end
+  end
+
+  def listar_socios_por_clase(socios, clase) do
+    socios_filtrados =
+      socios
+      |>Map.values()
+      |>Enum.filter(fn socio -> Enum.member?(socio.clases, clase)end)
+
+      {:ok, socios_filtrados}
+  end
 end
